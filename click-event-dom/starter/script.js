237d14cb2cded74ba8e417720f1e48b3ea4ed169
daffaalex22 <daffaalex22@gmail.com>
2025-12-13 00:24:01 +0700

    feat: add new dom project starter

---
+++ click-event-dom/starter/script.js
@@ -0,0 +1,14 @@
+// Click Event Starter
+// Add your click event listeners here
+
+// Get references to the buttons and message element
+// TODO: Add some DOM getters here
+
+// Add click event listener to btn1
+// TODO: Add some event listeners here
+
+// TODO: Add a click event listener to the document body
+// When clicked anywhere on the page, change the background color
+
+// TODO: Add a click event listener to all buttons at once
+// When any button is clicked, log to the console which button was clicked
